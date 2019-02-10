053a1cee5dcfad0a259a81072b6b852e544b7010
adrianB3 <balanescuadrian71@gmail.com>
2019-02-10 20:47:13 +0200

    added fourier transform

--- convolution_filters_edge_det/fourier_transform.py
+++ convolution_filters_edge_det/fourier_transform.py
@@ -9,6 +9,39 @@ image_stripes = cv2.cvtColor(image_stripes, cv2.COLOR_BGR2RGB)
 image_solid = cv2.imread('images/pink_solid.jpg')
 image_solid = cv2.cvtColor(image_solid, cv2.COLOR_BGR2RGB)
 
-f, (ax1, ax2) = plt.subplot(1, 2, figsize=(10, 5))
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 ax1.imshow(image_stripes)
 ax2.imshow(image_solid)
+
+# convert to gray
+gray_stripes = cv2.cvtColor(image_stripes, cv2.COLOR_RGB2GRAY)
+gray_solid = cv2.cvtColor(image_solid, cv2.COLOR_RGB2GRAY)
+
+# normalize images to [0,1]
+norm_stripes = gray_stripes/255.0
+norm_solid = gray_solid/255.0
+
+
+def ft_image(norm_image):
+    f = np.fft.fft2(norm_image)
+    fshift = np.fft.fftshift(f)
+    frequency_tx = 20 * np.log(np.abs(fshift))
+
+    return frequency_tx
+
+
+f_stripes = ft_image(norm_stripes)
+f_solid = ft_image(norm_solid)
+
+f, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20, 10))
+
+ax1.set_title('original image')
+ax1.imshow(image_stripes)
+ax2.set_title('frequency transform image')
+ax2.imshow(f_stripes, cmap='gray')
+
+ax3.set_title('original image')
+ax3.imshow(image_solid)
+ax4.set_title('frequency transform image')
+ax4.imshow(f_solid, cmap='gray')
+plt.show()
