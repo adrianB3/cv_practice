d0731074e289a484ab72436a07dd985cc8b037c0
adrianB3 <balanescuadrian71@gmail.com>
2019-02-21 10:21:42 +0200

    added first tf tutorial

---
+++ tensorflow_tutorial/simple_linear_model/simple_linear_model.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+print (tf.__version__)
+
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets('data\\MNIST\\')
+
+print("Size of: ")
+print(" - Training-set:\t\t{}".format(data.train)
+print(" - Validation-set:\t{}".format(data.num_val))
+print(" - Test-set:\t\t{}".format(data.num_test))
+
+img_size_flat = data.img_size_flat
+img_shape = data.img_shape
+num_classes = data.num_classes
+# TODO data import not working
